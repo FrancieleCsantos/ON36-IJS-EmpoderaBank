70dd1c11266cdb90a89ac4123948eb27ac62b1f9
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2024-07-17 22:26:55 -0300

    Criado classe ContaBancária, e validações para saque, deposito e transferência

--- src/Conta/contaBancaria.ts
+++ src/Conta/contaBancaria.ts
@@ -1,8 +1,35 @@
-interface ContaBancaria{
-    numerDaConta: number;
+interface ContaBancaria {
+    numeroDaConta: number;
     saldo: number;
     depositar(valor: number): void;
-    sacar(valor: number):boolean;
-    transferir(valor: number, contaDestino:ContaBancaria):boolean;
+    sacar(valor: number): boolean;
+    transferir(valor: number, contaDestino: ContaBancaria): boolean;
 }
 
+abstract class ContaBancariaImpl implements ContaBancaria {
+
+    constructor(
+        public numeroDaConta: number,
+        public saldo: number = 0
+    ) { }
+
+    depositar(valor: number): void {
+        this.saldo += valor;
+    }
+
+    sacar(valor: number): boolean {
+        if (valor <= this.saldo) {
+            this.saldo -= valor
+            return true
+        }
+        return false
+    }
+
+    transferir(valor: number, contaDestino: ContaBancaria): boolean {
+        if (this.sacar(valor)) {
+            contaDestino.depositar(valor);
+            return true
+        }
+        return false
+    }
+}
